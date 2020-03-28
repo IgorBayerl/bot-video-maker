82b011c81ca580a88938198998f6b99ce90f5339
Igor Bayerl <bayerl.igor@gmail.com>
2020-03-28 16:00:47 -0300

    add seach term and prefix

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-
+const readline = require('readline-sync')
 
 function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm() {
-        return 'TERM EXAMPLE'
+        return readline.question('Type a Wikipedia search term : ') ///// Questionando o termo
+    }
+
+    ///// questionando o prefixo
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+
+        return selectedPrefixText
     }
 
     console.log(content)
