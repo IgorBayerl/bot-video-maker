e011e1152a8530c1315b5e47a2754d576c96456c
Igor Bayerl <bayerl.igor@gmail.com>
2020-03-28 15:46:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+
+    function askAndReturnSearchTerm() {
+        return 'TERM EXAMPLE'
+    }
+
+    console.log(content)
+}
+
+start()
